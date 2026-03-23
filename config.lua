ce350ca76d99cb6381423ec11b63eb1925fe9d37
LucasRodriguesaa <61474992+LucasRodriguesaa@users.noreply.github.com>
2026-03-23 02:58:03 -0400

    Criar config.lua

---
+++ config.lua
@@ -0,0 +1,32 @@
+Config = {}
+
+Config.Dominacao = {
+    {
+        id = 1, 
+        nome = "Morro do Vagos",
+        pos = { x = 116.6, y = -1949.8, z = 20.7 }, 
+        raioSpawn = 25.0,
+        quantidadeNPCs = 15,
+        grupoParaSetar = "LiderVagos", 
+        permPermitida = "vagos.permissao",
+        npcHash = "g_m_y_mexgoon_01", 
+        armaNPC = "WEAPON_MICROSMG",
+        precisaoNPC = 80,
+        vidaNPC = 400,
+        coleteNPC = 100
+    },
+    {
+        id = 2, 
+        nome = "Mansão Cartel",
+        pos = { x = -2167.3, y = 5195.1, z = 16.8 }, 
+        raioSpawn = 35.0,
+        quantidadeNPCs = 20,
+        grupoParaSetar = "LiderCartel", 
+        permPermitida = "cartel.permissao",
+        npcHash = "s_m_m_chemsec_01", 
+        armaNPC = "WEAPON_CARBINERIFLE",
+        precisaoNPC = 90,
+        vidaNPC = 600,
+        coleteNPC = 100
+    }
+}
